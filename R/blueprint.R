6ed022bed30e8dbb8d17f48846fe21abc1affabe
Patrick Anker <patricksanker@gmail.com>
2020-06-22 13:31:05 -0400

    Blueprint documentation start

--- R/blueprint.R
+++ R/blueprint.R
@@ -1,11 +1,26 @@
-blueprint <- function(name, 
+#' Create a blueprint
+#'
+#' @param name The name of the blueprint
+#' @param command The code to build the target dataset
+#' @param description An optional description of the dataset to be used for
+#'                    codebook generation
+#' @param metadata The associated variable metadata for this dataset
+#' @param export_metadata Indicator to render new metadata file for new dataset
+#' @param metadata_file_type
+#' @param metadata_file_path
+#' @param ...
+#' @param class
+#'
+#' @return
+#' @export
+blueprint <- function(name,
                       command,
                       description = NULL,
                       metadata = NULL,
                       export_metadata = TRUE,
                       metadata_file_type = c("csv"),
                       metadata_file_path = here::here("blueprints"),
-                      ..., 
+                      ...,
                       class = character()) {
   stopifnot(is.character(name))
   stopifnot(is.null(description) || is.character(description))
@@ -35,66 +50,82 @@ capture_command <- function(quoted_statement) {
   quoted_statement
 }
 
+#' @export
 blueprint_target_name <- function(x, ...) {
   UseMethod("blueprint_target_name")
 }
 
+#' @export
 blueprint_target_name.default <- function(x) {
   bp_err("Not defined")
 }
 
+#' @export
 blueprint_target_name.character <- function(x) {
   paste0(x, "_initial")
 }
 
+#' @export
 blueprint_target_name.blueprint <- function(blueprint) {
   blueprint_target_name(blueprint$name)
 }
 
+#' @export
 blueprint_checks_name <- function(x, ...) {
   UseMethod("blueprint_checks_name")
 }
 
+#' @export
 blueprint_checks_name.default <- function(x, ...) {
   bp_err("Not defined")
 }
 
+#' @export
 blueprint_checks_name.character <- function(x) {
   paste0(x, "_checks")
 }
 
+#' @export
 blueprint_checks_name.blueprint <- function(blueprint) {
   blueprint_checks_name(blueprint$name)
 }
 
+#' @export
 blueprint_final_name <- function(x, ...) {
   UseMethod("blueprint_final_name")
 }
 
+#' @export
 blueprint_final_name.default <- function(x, ...) {
   bp_err("Not defined")
 }
 
+#' @export
 blueprint_final_name.character <- function(x) {
   x
 }
 
+#' @export
 blueprint_final_name.blueprint <- function(blueprint) {
   blueprint_final_name(blueprint$name)
 }
 
+#' @export
 blueprint_reference_name <- function(x, ...) {
   UseMethod("blueprint_reference_name")
 }
 
+#' @export
 blueprint_reference_name.default <- function(x, ...) {
   bp_err("Not defined")
 }
 
+#' @export
 blueprint_reference_name.character <- function(x) {
   paste0(x, "_blueprint")
 }
 
+#' @export
 blueprint_reference_name.blueprint <- function(blueprint) {
   blueprint_reference_name(blueprint$name)
 }
--- R/blueprintr-package.R
+++ R/blueprintr-package.R
@@ -1,4 +1,4 @@
-#' @importFrom rlang abort call2 call_name call_ns dots_list eval_tidy expr is_symbol is_syntactic_literal list2 node_car node_cdr node_caar node_cadr node_cdar
+#' @importFrom rlang abort call2 call_name call_ns dots_list eval_tidy expr is_symbol is_syntactic_literal list2 node_car node_cdr node_caar node_cadr node_cdar node_cddr
 #' @import data.table
 #' @importFrom glue glue glue_collapse
 "_PACKAGE"
