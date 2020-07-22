5c62170dfbd21d645960454b27bb381c894354ff
Patrick Anker <patricksanker@gmail.com>
2020-07-22 17:02:47 -0400

    Add labelled cleanup feature
    + Added cleanup details to blueprint() docs
    ∆ Some restructuring of cleanup features

---
+++ R/default-cleanup-features.R
@@ -0,0 +1,22 @@
+has_dropped_feature <- function(blueprint, meta) {
+  "dropped" %in% names(meta) &&
+    !identical(blueprint$drop_columns, FALSE)
+}
+
+drop_columns <- function(df, blueprint, meta) {
+  dropped_cols <- meta[!is.na(meta$dropped) & meta$dropped == TRUE, "name", drop = TRUE]
+
+  if (length(dropped_cols) > 0) {
+    df <- dplyr::select(df, -dropped_cols)
+  }
+
+  df
+}
+
+has_reorder_feature <- function(blueprint, meta) {
+  !identical(blueprint$reorder_columns, FALSE)
+}
+
+reorder_columns <- function(df, blueprint, meta) {
+  df[, meta$name]
+}
