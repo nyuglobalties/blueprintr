0c6d4a23676c09647d1a6a730cbdad23e4f98c8e
Patrick Anker <patricksanker@gmail.com>
2020-12-16 10:57:40 -0500

    Simplify leaf node check

--- R/ast.R
+++ R/ast.R
@@ -4,14 +4,16 @@ extract_ast <- function(expr) {
   }
 
   # Base cases
-  if (is_syntactic_literal(expr) || is_symbol(expr)) {
+  if (is_leaf(expr)) {
     return(expr)
   }
 
   # Handle translation of AST
   if (is_ast(expr)) {
     if (is_function_ast(expr)) {
-      expr$fargs <- lapply(expr$fargs, extract_ast)
+      if (any(mutable_fargs(expr))) {
+        mutable_fargs(expr) <- lapply(mutable_fargs(expr$fargs), extract_ast)
+      }
     }
 
     expr$args <- lapply(expr$args, extract_ast)
@@ -39,7 +41,10 @@ modify_ast_if <- function(ast, .p, .f, ..., recurse = TRUE) {
 
     if (isTRUE(recurse) && is_ast(out)) {
       if (is_function_ast(out)) {
-        out$fargs <- lapply(out$fargs, modify_ast_if, .p, .f, ...)
+        if (any(mutable_fargs(out))) {
+          mutable_fargs(out) <-
+            lapply(mutable_fargs(out$fargs), modify_ast_if, .p, .f, ...)
+        }
       }
 
       out$args <- lapply(out$args, modify_ast_if, .p, .f, ...)
@@ -79,7 +84,7 @@ collapse_ast <- function(ast) {
 
   if (is_function_ast(ast)) {
     fargs <- lapply(ast$fargs, collapse_ast)
-    body <- collapse_ast(ast$args)
+    body <- trim_leaves(collapse_ast(ast$args))
 
     return(call(ast$head, as.pairlist(fargs), body))
   }
@@ -97,6 +102,16 @@ collapse_ast <- function(ast) {
   call2(ast$head, !!!collapsed_args)
 }
 
+trim_leaves <- function(x) {
+  if (length(x) == 1) {
+    if (is_leaf(x[[1]])) {
+      x <- x[[1]]
+    }
+  }
+
+  x
+}
+
 collapse_qualified_ast <- function(ast) {
   collapsed_args <- lapply(ast$args, collapse_ast)
 
@@ -109,40 +124,44 @@ collapse_qualified_ast <- function(ast) {
   }
 }
 
-ast <- function(call) {
-  if (identical(call_name(call), "function")) {
-    return(function_ast(call)) 
+ast <- function(.call) {
+  if (is_leaf(.call)) {
+    return(.call)
   }
 
-  if (is_qualified_call(call)) {
-    return(qualified_ast(call))
+  if (identical(call_name(.call), "function")) {
+    return(function_ast(.call))
+  }
+
+  if (is_qualified_call(.call)) {
+    return(qualified_ast(.call))
   }
 
   structure(
     list(
-      head = call_name(call),
-      args = as.list(call)[-1]
+      head = call_name(.call),
+      args = as.list(.call)[-1]
     ),
     class = "ast"
   )
 }
 
-qualified_ast <- function(call) {
-  qual_sym <- qualifier(call)
-  qual_head <- qualified_head(call)
-  namespace <- if (is_namespaced_call(call)) {
-    call_ns(call)
+qualified_ast <- function(.call) {
+  qual_sym <- qualifier(.call)
+  qual_head <- qualified_head(.call)
+  namespace <- if (is_namespaced_call(.call)) {
+    call_ns(.call)
   } else {
     NULL
   }
 
   structure(
     list(
-      head = call_name(call),
+      head = call_name(.call),
       qual_head = qual_head,
       qual_sym = qual_sym,
       ns = namespace,
-      args = as.list(call)[-1]
+      args = as.list(.call)[-1]
     ),
     class = c("ast", "qualified_ast")
   )
@@ -152,34 +171,38 @@ is_language <- function(x) {
   identical(typeof(x), "language")
 }
 
-qualifier <- function(call) {
-  stopifnot(is_language(call))
-
-  node_caar(call)
+is_leaf <- function(x) {
+  is_syntactic_literal(x) || is_symbol(x)
 }
 
-qualified_head <- function(call) {
-  stopifnot(is_language(call))
+qualifier <- function(.call) {
+  stopifnot(is_language(.call))
 
-  extract_ast(node_car(node_cdar(call)))
+  node_caar(.call)
 }
 
-is_qualified_call <- function(call) {
-  if (!is_language(call)) {
+qualified_head <- function(.call) {
+  stopifnot(is_language(.call))
+
+  extract_ast(node_car(node_cdar(.call)))
+}
+
+is_qualified_call <- function(.call) {
+  if (!is_language(.call)) {
     return(FALSE)
   }
 
-  if (!is_language(node_car(call))) {
+  if (!is_language(node_car(.call))) {
     return(FALSE)
   }
 
-  call_cmd <- node_car(call)
+  call_cmd <- node_car(.call)
 
   if (!is_symbol(node_cadr(node_cdr(call_cmd)))) {
     return(FALSE)
   }
 
-  qual_sym <- qualifier(call)
+  qual_sym <- qualifier(.call)
 
   identical(qual_sym, quote(`::`)) ||
     identical(qual_sym, quote(`:::`)) ||
@@ -187,28 +210,61 @@ is_qualified_call <- function(call) {
     identical(qual_sym, quote(`@`))
 }
 
-is_namespaced_call <- function(call) {
-  if (!is_qualified_call(call)) {
+is_namespaced_call <- function(.call) {
+  if (!is_qualified_call(.call)) {
     return(FALSE)
   }
 
-  qual_sym <- qualifier(call)
+  qual_sym <- qualifier(.call)
 
   identical(qual_sym, quote(`::`)) ||
     identical(qual_sym, quote(`:::`))
 }
 
-function_ast <- function(call) {
-  call_list <- as.list(call)
+function_ast <- function(.call) {
+  call_list <- as.list(.call)
 
   structure(
     list(
-      head = call_name(call),
+      head = call_name(.call),
       fargs = as.list(call_list[[2]]),
-      args = ast(call_list[[3]])
+      args = call_list[[3]]
     ),
     class = c("ast", "function_ast")
   )
+}
+
+mutable_fargs <- function(x) {
+  UseMethod("mutable_fargs", x)
+}
+
+has_mutable_fargs <- function(x) {
+  if (!is_function_ast(x)) {
+    return(FALSE)
+  }
+
+  any()
+}
+
+update_mutable_fargs <- function(x, .f, ...) {
+
+}
+
+mutable_fargs.default <- function(x) {
+  bp_err("Not defined in general")
+}
+
+mutable_fargs.function_ast <- function(x) {
+  vlapply(x$fargs, function(farg) !is.symbol(farg))
+}
+
+mutable_fargs.list <- function(x) {
+  x[vlapply(x, function(farg) !is.symbol(farg))]
+}
+
+`mutable_fargs<-` <- function(x, vals) {
+  x$fargs[mutable_fargs(x)] <- vals
+  x
 }
 
 is_ast <- function(x) {
--- tests/testthat/test-ast.R
+++ tests/testthat/test-ast.R
@@ -13,16 +13,19 @@ test_that("extract_ast and collapse_ast are inverses", {
   })
   expr3 <- bquote(codetools::showTree(expr1))
 
+  expr4 <- quote(func(function(.x) .x, param = "test"))
+  expr5 <- quote(func(function(.x, .p = "word") .x, param = "test"))
+
   ast_ident <- function(e) collapse_ast(extract_ast(e))
 
   expect_equal(ast_ident(expr1), expr1)
   expect_equal(ast_ident(expr2), expr2)
   expect_equal(ast_ident(expr3), expr3)
+  expect_equal(ast_ident(expr4), expr4)
+  expect_equal(ast_ident(expr5), expr5)
 })
 
 test_that("Corner cases are covered", {
-  expect_error(exptract_ast(bquote()))
-
   some_obj <- list(x = 1, y = letters)
   expect_identical(collapse_ast(some_obj), some_obj)
 })
